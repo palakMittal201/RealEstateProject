5c2e02859c2bc4b117abb162c9591cf9db3969f1
palakMittal201 <cse22069@glbitm.ac.in>
2024-12-03 01:18:08 +0530

    Add files via upload

---
+++ src/assets/assets.js
@@ -0,0 +1,101 @@
+import logo from './logo.svg'
+import logo_dark from './logo_dark.svg'
+import cross_icon from './cross_icon.svg'
+import menu_icon from './menu_icon.svg'
+import star_icon from './star_icon.svg'
+import left_arrow from './left_arrow.svg'
+import right_arrow from './right_arrow.svg'
+import header_img from './header_img.png'
+import brand_img from './brand_img.png'
+import project_img_1 from './project_img_1.jpg'
+import project_img_2 from './project_img_2.jpg'
+import project_img_3 from './project_img_3.jpg'
+import project_img_4 from './project_img_4.jpg'
+import project_img_5 from './project_img_5.jpg'
+import project_img_6 from './project_img_6.jpg'
+import profile_img_1 from './profile_img_1.jpg'
+import profile_img_2 from './profile_img_2.jpg'
+import profile_img_3 from './profile_img_3.jpg'
+
+export const assets = {
+    logo,
+    logo_dark,
+    cross_icon,
+    menu_icon,
+    star_icon,
+    header_img,
+    brand_img,
+    project_img_1,
+    project_img_2,
+    project_img_3,
+    project_img_4,
+    left_arrow,
+    right_arrow,
+}
+
+export const projectsData = [
+    {
+      title: "Skyline Haven",
+      price: "₹2,89,50,000",
+      location: "New Delhi",
+      image: project_img_1
+    },
+    {
+      title: "Vista Verde",
+      price: "₹28,50,000",
+      location: "Gurgaon",
+       image: project_img_2
+    },
+    {
+      title: "Serenity Suites",
+      price: "₹82,50,000",
+      location: "Omega3, Omicron",
+       image: project_img_3
+    },
+    {
+      title: "Central Square",
+      price: "₹2,56,50,000",
+      location: "Lucknow",
+       image: project_img_4
+    },
+    {
+      title: "Vista Verde",
+      price: "₹27,50,000",
+      location: "Gaur City2",
+       image: project_img_5
+    },
+    {
+      title: "Serenity Suites",
+      price: "₹52,50,000",
+      location: "SurajPur,Gr.Noida",
+       image: project_img_6
+    },
+    
+  ];
+
+  export const testimonialsData = [
+    {
+        name: "Nihal Rasool",
+        title: "Marketing Manager",
+        image: profile_img_1,
+        alt: "Portrait of Nihal Rasool",
+        rating: 5,
+        text: "From the very first meeting, they understood my vision and helped me find the perfect property. Their attention to detail and commitment to client satisfaction is unmatched."
+    },
+    {
+        name: "Neha Rajvanshi",
+        title: "UI/UX Designer",
+        image: profile_img_2,
+        alt: "Portrait of Neha Rajvanshi",
+        rating: 5,
+        text: "From the very first meeting, they understood my vision and helped me find the perfect property. Their attention to detail and commitment to client satisfaction is unmatched."
+    },
+    {
+        name: "Palak Mittal",
+        title: "Co-Founder",
+        image: profile_img_3,
+        alt: "Portrait of Palak Mittal",
+        rating: 5,
+        text: "From the very first meeting, they understood my vision and helped me find the perfect property. Their attention to detail and commitment to client satisfaction is unmatched."
+    }
+];
